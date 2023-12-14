d75d241e0ef4dd7b29b21be687776c059bacef72
Kevin H Wilson <khwilson@gmail.com>
2023-12-14 10:03:07 -0600

    fix README

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Advent of Code 2023
+
+Kevin's solutions to Advent of Code 2023 problems.
+
+## Requirements
+
+These were written with Python 3.11 and with dependencies managed with poetry. You can run
+
+```bash
+poetry install
+```
+
+to get started.
+
+## Running
+
+To run the code for a particular day, use
+
+```bash
+poetry run advent DAY
+```
+
+where `DAY` is the day you want to run.
+
+Alternatively, you can run all days with
+
+```bash
+poetry run advent all
+```
+
+## License
+
+MIT
--- src/advent/cli.py
+++ src/advent/cli.py
@@ -8,12 +8,21 @@ import click
 @click.option("--input-file", default=None)
 def cli(day: str, input_file: str):
     """Runner for AOC 2023 solutions"""
-    module_name = f"day{int(day):02d}"
-    mod = importlib.import_module(f".{module_name}", "advent.solutions")
+    if day.lower() == "all":
+        mods = [f"day{nday:02d}" for nday in range(1, 26)]
+    else:
+        mods = [f"day{int(day):02d}"]
 
-    input_file = input_file or f"data/{module_name}.txt"
-    mod.part1(input_file)
-    mod.part2(input_file)
+    for module_name in mods:
+        print(f"\n~~~ {module_name} ~~~")
+        try:
+            mod = importlib.import_module(f".{module_name}", "advent.solutions")
+        except ImportError:
+            print(f"{module_name} not available yet")
+
+        _input_file = input_file or f"data/{module_name}.txt"
+        mod.part1(_input_file)
+        mod.part2(_input_file)
 
 
 if __name__ == "__main__":
